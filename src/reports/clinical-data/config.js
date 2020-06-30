92081344c2805e8037bd3a6ddec1117c44cbd49e
evans-g-crsj <eg.crsj@gmail.com>
2020-06-30 17:17:28 -0400

    fix: add temporary patch to remove not tagged dxs + change accessor for one column

--- src/reports/clinical-data/config.js
+++ src/reports/clinical-data/config.js
@@ -9,7 +9,7 @@ const participants = {
     { field: 'is_proband' },
     { field: 'study.short_name' },
     { field: 'family.family_compositions.composition' },
-    { field: 'diagnoses.diagnosis_category' },
+    { field: 'diagnosis_category' },
     { field: 'gender' },
     { field: 'race' },
     { field: 'ethnicity' },
