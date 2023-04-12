462d4accdd1cd1d38bdd886c34585ee24d613747
evans-g-crsj <eg.crsj@gmail.com>
2023-04-12 17:47:28 -0400

    :bug: Add correct headers to file manifest

--- src/reports/file-manifest/config.ts
+++ src/reports/file-manifest/config.ts
@@ -1,28 +1,55 @@
 import { formatFileSize } from '../../utils/formatFileSize';
 import { SheetConfig } from '../types';
 
+type BiospecimenData = { family?: { family_id?: string }; sample_id?: string; external_sample_id?: string };
+type ParticipantsData = {
+    biospecimens: BiospecimenData[];
+}[];
+const processBiospecimens = (participants: ParticipantsData, key: keyof BiospecimenData): string[] =>
+    (participants || [])
+        .map(x => x?.biospecimens || [])
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        .flat()
+        .map(x => (key === 'family' ? x?.family?.family_id : x[key]))
+        .filter(x => !!x);
+
 const config: SheetConfig = {
     sheetName: 'Files',
     root: 'file',
     columns: [
-        { field: 'file_id', header: 'File ID' },
-        { field: 'file_name', header: 'File Name' },
-        { field: 'file_size', header: 'File Size', transform: value => formatFileSize(value, { output: 'string' }) },
-        { field: 'data_category', header: 'Data Category' },
+        { field: 'access_urls', header: 'Access URL' },
+        { field: 'study.data_category', header: 'Data Category' },
         { field: 'data_type', header: 'Data Type' },
+        { field: 'sequencing_experiment.experiment_strategy', header: 'Experimental Strategy' },
+        //TODO not implemented  { field: '--', header: 'External Collection ID' },
+        //TODO not implemented   { field: '--', header: 'External Participant ID' },
+        {
+            field: 'participants',
+            header: 'External Sample ID',
+            transform: participants => processBiospecimens(participants, 'external_sample_id'),
+        },
+        {
+            field: 'participants',
+            header: 'Family ID',
+            transform: participants => processBiospecimens(participants, 'family'),
+        },
         { field: 'file_format', header: 'File Format' },
-        { field: 'sequencing_experiment.experimental_strategy', header: 'Experimental Strategy' },
-        { field: 'file_hash', header: 'Hash' },
-        { field: 'study.name', header: 'Study Name' },
+        {
+            field: 'file_id',
+            header: 'File ID',
+        },
+        { field: 'file_name', header: 'File Name' },
+        { field: 'size', header: 'File Size', transform: value => formatFileSize(value, { output: 'string' }) },
+        { field: 'hashes.md5', header: 'Hash' },
         { field: 'participants.participant_id', header: 'Participant ID' },
-        { field: 'biospecimens.sample_id', header: 'Sample ID' },
-        { field: 'biospecimens.biospecimen_id', header: 'Biospecimen ID' },
-        { field: 'participants.family_relationships.family_type', header: 'Participant Type' },
-        { field: 'participants.family_id', header: 'Family ID' },
-        { field: 'participants.submitter_participant_id', header: 'Submitter Participant ID' },
-        { field: 'biospecimens.submitter_sample_id', header: 'Submitter Sample ID' },
-        { field: 'biospecimens.submitter_biospecimen_id', header: 'Submitter Biospecimen ID' },
-        { field: 'sequencing_experiment.bio_informatic_analysis', header: 'Analysis ID' },
+        { field: 'participants', header: 'Proband', transform: xs => xs.map(x => (x ? 'Yes' : 'No')).join(',') },
+        {
+            field: 'participants',
+            header: 'Sample ID',
+            transform: participants => processBiospecimens(participants, 'sample_id'),
+        },
+        { field: 'study.study_name', header: 'Study Name' },
     ],
     sort: [{ file_id: { order: 'asc' } }],
 };
